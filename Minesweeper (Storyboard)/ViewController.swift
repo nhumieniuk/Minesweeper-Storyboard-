e3295af6c6eebd5d4c125cbb4a93262df52b3f0c
Natan Humieniuk <nhumieniuk@bsd220.org>
2021-04-29 08:53:44 -0500

    added flagging

--- Minesweeper (Storyboard)/ViewController.swift
+++ Minesweeper (Storyboard)/ViewController.swift
@@ -9,14 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
-    
     var easy = false
     var medium = false
     var hard = false
     var easySquares = [Int](repeating: 0, count: 64)
     var mediumSquares = [Int](repeating: 0, count: 256)
     var hardSquares = [Int](repeating: 0, count: 480)
+    var flag = false
     var yIncrement = 0.0
     var index = 0
     private var placedSquare: UIButton!
@@ -34,6 +33,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         easy = true
+        flag = true
         randomMines()
         calculateNumberedSquares()
         printGrid()
@@ -49,33 +49,23 @@ class ViewController: UIViewController {
     
     func printGrid()
     {
+        
         //prints a grid
         for square in easySquares
         {
-            
             let placedSquare = UIButton(frame: CGRect(x: 10 + ((index % 8) * 30), y: 100 + Int(yIncrement) * 30, width: 30, height: 30))
+            if(easySquares[index] >= 9)
+            {
+            print("mine at " + String(index))
+            }
             yIncrement += 0.125
             index += 1
-            placedSquare.backgroundColor=UIColor.lightGray
+            placedSquare.backgroundColor = UIColor.darkGray
             placedSquare.layer.borderWidth=1
             placedSquare.layer.borderColor = UIColor.black.cgColor
             placedSquare.tag = square
             placedSquare.addTarget(self, action: #selector(squareTapped), for: .touchUpInside)
             self.view.addSubview(placedSquare)
-            if(square >= 9)
-            {
-                placedSquare.backgroundColor=UIColor.red
-            }
-            else{
-                //code for numbered squares
-                if(square != 0)
-                {
-                    placedSquare.setTitle(String(square), for: UIControl.State.normal)
-                    self.view.addSubview(placedSquare)
-                }
-            }
-                
-            
         }
     }
     
@@ -87,15 +77,16 @@ class ViewController: UIViewController {
             var mines = 10
             while(mines > 0)
             {
-                if(easySquares[i % 63] == 0)
+                if(easySquares[i % 64] == 0)
                 {
-                    i += 1
                     let random = Int.random(in: 1...64)
                     if(random == 1)
                     {
-                        easySquares[i % 63] = 9 // 9 means bomb
+                        easySquares[i % 64] = 9 // 9 means bomb
                         mines -= 1
+                        print(i)
                     }
+                    i += 1
                 }
                 else{
                     i += 1
@@ -155,7 +146,7 @@ class ViewController: UIViewController {
     {
         //1,2,3,4,5,6,7,8,15,16,23,24,31,32,39,40,47,48,53,57,58,59,60,61,62
         //0, 7, 56, 63
-        for i in 0...63
+        for i in 0 ..< easySquares.count
         {
             if easySquares[i] >= 9
             {
@@ -236,8 +227,51 @@ class ViewController: UIViewController {
         }
         
     }
+    
+    func loseGame()
+    {
+        index = 0
+        yIncrement = 0
+        //code for losing the game here
+        for square in easySquares
+        {
+            
+            let placedSquare = UIButton(frame: CGRect(x: 10 + ((index % 8) * 30), y: 100 + Int(yIncrement) * 30, width: 30, height: 30))
+            index += 1
+            yIncrement += 0.125
+            if(square >= 9)
+            {
+                placedSquare.backgroundColor = UIColor.red
+                placedSquare.layer.borderWidth=1
+                placedSquare.layer.borderColor = UIColor.black.cgColor
+                placedSquare.setTitle("💣", for: .normal)
+                self.view.addSubview(placedSquare)
+            }
+        }
+        print("lose")
+    }
     @objc func squareTapped(sender: UIButton){
-        print(sender.tag)
+        if(flag == false){
+        sender.backgroundColor = UIColor.lightGray
+        if(sender.tag >= 9){
+            loseGame()
+            sender.backgroundColor = UIColor.red
+        }
+        if(sender.tag > 0 && sender.tag < 9){
+            sender.setTitle(String(sender.tag), for: .normal)
+        }
+        }
+        else{
+            if(sender.backgroundColor == UIColor.darkGray)
+            {
+                if(sender.currentTitle == nil){
+                sender.setTitle("🚩", for: .normal)
+                }
+                else{
+                    sender.setTitle(nil, for: .normal)
+                }
+            }
+        }
     }
 }
 
